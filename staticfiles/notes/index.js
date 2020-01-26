fe99d605c8a8a8aab57ef817598af45ec45ec795
thisisshub <thisisshub@gmail.com>
2020-01-26 10:39:43 +0530

    heroku deployment

--- staticfiles/notes/index.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-      $(".diagonal-bg svg line").attr("stroke-width",  ((30 + scroll/10)  + "%"));
-      //30 is the starting width
-      //alter the amount of growth by changing scroll/x
-  });
-  
-  
