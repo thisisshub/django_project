89c332569243a2fe1c88ee66be8606d9053037ba
thisisshub <thisisshub@gmail.com>
2020-09-14 15:58:54 +0530

    Modify login page

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
