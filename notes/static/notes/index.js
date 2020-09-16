4126bc065b7f29d78f679fc14443cb6184465e8d
thisisshub <thisisshub@gmail.com>
2020-09-16 12:47:17 +0530

    Modify navigation button with bootstrap
    This commit contains,
    - Change of not user_authenticated button to outline bootstrap buttons
    - Change of if user_authenticated button to primary buttons
    - Change pagination position to center

--- notes/static/notes/index.js
+++ notes/static/notes/index.js
@@ -4,5 +4,32 @@ $(window).scroll(function() {
       //30 is the starting width
       //alter the amount of growth by changing scroll/x
   });
+
+  $(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+       isClosed = false;
   
+      trigger.click(function () {
+        hamburger_cross();      
+      });
   
+      function hamburger_cross() {
+  
+        if (isClosed == true) {          
+          overlay.hide();
+          trigger.removeClass('is-open');
+          trigger.addClass('is-closed');
+          isClosed = false;
+        } else {   
+          overlay.show();
+          trigger.removeClass('is-closed');
+          trigger.addClass('is-open');
+          isClosed = true;
+        }
+    }
+    
+    $('[data-toggle="offcanvas"]').click(function () {
+          $('#wrapper').toggleClass('toggled');
+    });  
+  });
